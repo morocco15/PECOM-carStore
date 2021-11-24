323a0cc47d0bec9d8226a0f5b4d0107951457e78
troubadourjimmy <zhangxubeinicolas@gmail.com>
2021-11-24 19:15:04 +0100

    modification valider commande dans panier

---
+++ src/main/webapp/app/product-container/product-container.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-product-container',
+  templateUrl: './product-container.component.html',
+  styleUrls: ['./product-container.component.scss']
+})
+export class ProductContainerComponent implements OnInit {
+
+  constructor() 
+  {
+    //
+   }
+
+  ngOnInit(): void 
+  {
+    //
+  }
+
+  handlePay():void
+  {
+    //
+    
+  }
+
+}
