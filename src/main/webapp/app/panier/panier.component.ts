2b7982869a6b3a55295548622c66b814063bd844
m2gi-bouril <bibla646464@gmail.com>
2021-11-26 16:33:34 +0100

    Début de création d'un panier statique

--- src/main/webapp/app/panier/panier.component.ts
+++ src/main/webapp/app/panier/panier.component.ts
@@ -13,4 +13,22 @@ export class PanierComponent implements OnInit {
   ngOnInit(): void {
     //
   }
+
+  /*test(): void{
+    //var toutSelectionne = document.getElementById("toutselectionne");
+    //var voiture1 = document.getElementById("voiture1");
+    //var voiture2 = document.getElementById("voiture2");
+    //var toutSelectionne = document.querySelector('input[value="toutselectionne"]');
+    var voiture1 = document.querySelector('input[value="voiture1"]');
+    var voiture2 = document.querySelector('input[value="voiture2"]');
+    //if (document.querySelector('input[value="toutselectionne"]').checked === true){
+    if (document.getElementById("toutselectionne").checked === true){
+      voiture1.checked=true;
+      voiture2.checked=true;
+    }
+    else{
+      voiture1.checked=false;
+      voiture2.checked=false;
+    }
+  }*/
 }
