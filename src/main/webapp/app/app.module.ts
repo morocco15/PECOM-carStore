d7b48fe673733de4afd2d873142cd59dee4278d8
m2gi-bouril <bibla646464@gmail.com>
2021-11-26 11:03:03 +0100

    création de la premiere fiche produit dans la page fil d'actualité

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -25,6 +25,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
 import { FildactualiteComponent } from './fildactualite/fildactualite.component';
+import { PanierComponent } from './panier/panier.component';
 
 @NgModule({
   imports: [
@@ -45,7 +46,7 @@ import { FildactualiteComponent } from './fildactualite/fildactualite.component'
     { provide: NgbDateAdapter, useClass: NgbDateDayjsAdapter },
     httpInterceptorProviders,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent, PanierComponent],
   bootstrap: [MainComponent],
 })
 export class AppModule {
