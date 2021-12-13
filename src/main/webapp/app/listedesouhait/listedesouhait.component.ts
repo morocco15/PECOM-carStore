ee93dd4c27281fe6f39875bc0c5401be04f99b60
m2gi-bouril <bibla646464@gmail.com>
2021-12-13 15:35:22 +0100

    réparation des boutons qui dépassent dans le panier et commencement de la page dynamique liste de souhait

--- src/main/webapp/app/listedesouhait/listedesouhait.component.ts
+++ src/main/webapp/app/listedesouhait/listedesouhait.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
+import { HttpClient } from '@angular/common/http';
+import { IVoiture } from 'app/entities/voiture/voiture.model';
+import { IPanier } from 'app/entities/panier/panier.model';
 
 @Component({
   selector: 'jhi-listedesouhait',
@@ -6,8 +15,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listedesouhait.component.scss'],
 })
 export class ListedesouhaitComponent implements OnInit {
+  voitures!: IVoiture[];
   constructor() {
     //
+  }
+
+  trackId(index: number, item: IPanier): number {
+    return item.id!;
   }
 
   ngOnInit(): void {
